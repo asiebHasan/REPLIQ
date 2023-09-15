998da4e84f9c28b1fd491902a02d0fbac2f658df
asiebHasan <asieb.hasan.supto@gmail.com>
2023-09-15 18:48:24 +0600

    admin panel

--- tracker/company/admin.py
+++ tracker/company/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Company
 # Register your models here.
-admin.site.register(Company)
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'email','is_active','is_staff']
+    search_fields = ['company_name']
